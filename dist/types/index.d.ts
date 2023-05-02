cba5eba57f3c73be4c0b7b3b60e1a149b6a0525a
Carlos <cadifilippo@gmail.com>
2023-05-01 22:50:59 -0300

    chore: add dist to gitignore

--- dist/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ITextShuffleProps } from './types';
-declare const shuffle: ({ text, duration, delay, delayResolve, fps, glyphs, animation, direction, onUpdate, onComplete, }: ITextShuffleProps) => void;
-export default shuffle;
--- dist/types/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const random: (arr: any) => any[];
-export declare const clamp01: (v: any) => any;
-export declare const quartOut: (t: any) => number;
-export declare const quadInOut: (t: any) => number;
-export declare const pick: (array: any) => any;
